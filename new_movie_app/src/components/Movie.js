dc0808db7e78f888804c116eadb730e77df29f9d
dahyeon.bae <dahyeon.bae@karfarm.com>
2021-10-17 19:27:08 +0900

    nomad

--- new_movie_app/src/components/Movie.js
+++ new_movie_app/src/components/Movie.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css'
-// Each child in a list should have a unique "key" prop.
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, title, year, rating , summary, poster, genres}) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-    <img src={poster} alt={title} title={title}></img>
-    <div className="movie__data">
-      <h2 className="movie__title">{title}</h2>
-      <h4 className="movie__year">{year}</h4>
-      <ul className="movie__genres">
-          {genres.map((genre, index)=> /* map gives you number arguments*/ (
-            <li key={index} className="genres__genre"> {genre} </li>))}
-      </ul>
-      <p className="movie__summary">{summary.slice(0, 200)} ...</p></div>
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres
+          }
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
 
-Movie.propTypes = { 
+Movie.propTypes = {
   id: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired
-}
+};
 
 export default Movie;
--- new_movie_app/src/routes/Home.js
+++ new_movie_app/src/routes/Home.js
@@ -1,49 +1,52 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from '../components/Movie';
-import './Home.css';
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
 
 class Home extends React.Component {
-  state = { //선언일 뿐 꼭 다 사용할 필요는 없다
-    isLoading : true,
+  state = {
+    isLoading: true,
     movies: []
-  }; 
-  getMovies = async () => { // 비동기 -> 기다리라고 명령함 (await )
-    const {data: {data: {movies}}} = 
-      await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading: false}); // this.setState({movies}); as same
-  }
-  componentDidMount(){ // render -> componentDidMount() 순서 
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
     this.getMovies();
   }
-  
   render() {
-    console.log("render refresh automatically");
     const { isLoading, movies } = this.state;
     return (
-      <section className="container"> 
-      {isLoading ? 
-      <div className="loader">
-        <span className="loader_text"> Loading now ... </span>
-      </div> : 
-      <div className="movies">
-        { movies.map(movie => { // variable movie
-            // console.log(movie);
-            return <Movie  // map() requires return always 
-              key= {movie.id}
-              id= {movie.id}
-              year ={movie.year}
-              title= {movie.title}
-              summary= {movie.summary}
-              rating= {movie.rating}
-              poster= {movie.medium_cover_image}
-              genres= {movie.genres}
-            />}
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
         )}
-      </div>
-      } </section>
-    )
+      </section>
+    );
   }
-}; 
+}
 
 export default Home;
